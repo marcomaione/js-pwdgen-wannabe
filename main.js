c26cc7622ae69bb5e75b32df2199c300f859994f
marco maione <maionemarco14@gmail.com>
2022-01-19 14:25:42 +0100

    esercizio finito

---
+++ main.js
@@ -0,0 +1,4 @@
+let name = prompt("inserisci il tuo nome");
+let surname = prompt("inserisci il tuo cognome");
+let color = prompt("inserisci il tuo colore preferito");
+document.getElementById("name").innerHTML = name + surname + color + "21"
